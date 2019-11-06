769c3cacc9797b9ad029a51678e653d9a43219d8
mike-albano <mike@mikealbano.com>
2019-11-06 11:37:32 -0700

    sync latest json updater

--- config_json-updater.py
+++ config_json-updater.py
@@ -1,3 +1,4 @@
+"""Simple python3 utility to re-write JSON files."""
 import json
 from boltons.iterutils import remap
 import sys
@@ -15,4 +16,8 @@ changed_values = remap(config_json, visit=change_val)
 drop_keys = lambda path, key, value: key not in bad_keys
 clean = remap(changed_values, visit=drop_keys)
 
+# Separators added for no spaces b/w key/val in JSON.
 print(json.dumps(clean, indent=2, separators=(',', ':')))  # Dump to stdout.
+out_file = open(sys.argv[1], 'w')  # Over-write the input file
+out_file.write(json.dumps(clean, indent=2, separators=(',', ':')))
+out_file.close()

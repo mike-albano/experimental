0ef61d5f64af198a96e54e214fd597c5831c4a2e
mike-albano <albanom@google.com>
2020-03-05 11:50:31 -0700

    works on directory of files.

--- config_json-updater.py
+++ config_json-updater.py
@@ -1,23 +1,37 @@
-"""Simple python3 utility to re-write JSON files."""
+"""Simple python3 utility to re-write JSON files.
+Example:
+python3 config_json-updater.py  json-files/
+"""
+
+import os
 import json
 from boltons.iterutils import remap
 import sys
 
-config_json = json.load(open(sys.argv[1], 'r'))
+# Change values in each of the files in a directory
+directory = sys.argv[1]  # Make sure to add a trailing "/" when running.
+
+# If you'd rather use one file, uncomment the following
+#config_json = json.load(open(sys.argv[1], 'r'))
 bad_keys = ['dot11r-method']
 
 def change_val(path, key, value):
   # Set dot11r config to false, if it's currently set to true.
-  if key == 'dot11r' and value == True:
-    return key, False
+  if key == 'dot11r' and value == False:
+    return key, True
   return key, value
 
-changed_values = remap(config_json, visit=change_val)
-drop_keys = lambda path, key, value: key not in bad_keys
-clean = remap(changed_values, visit=drop_keys)
+def iterate_files(directory):
+  """Itreate over files in the given directory and change values."""
+  for filename in os.listdir(directory):
+    if filename.endswith('.json'):
+      config_json = json.load(open(directory + filename, 'r'))
+      changed_values = remap(config_json, visit=change_val)
+      drop_keys = lambda path, key, value: key not in bad_keys
+      new_file = remap(changed_values, visit=drop_keys)
+      out_file = open(directory + filename, 'w')  # Over-write the input file
+      # Separators added for no spaces b/w key/val in JSON.
+      out_file.write(json.dumps(new_file, indent=2, separators=(',', ':')))
+      out_file.close()
 
-# Separators added for no spaces b/w key/val in JSON.
-print(json.dumps(clean, indent=2, separators=(',', ':')))  # Dump to stdout.
-out_file = open(sys.argv[1], 'w')  # Over-write the input file
-out_file.write(json.dumps(clean, indent=2, separators=(',', ':')))
-out_file.close()
+new_file = iterate_files(directory)
